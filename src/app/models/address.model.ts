b1e36bd3423f9efa8e303135a625975e1f165381
hhahhahh1 <hhahhahh@protonmail.com>
2024-06-16 11:07:59 +0300

    update config files

---
+++ src/app/models/address.model.ts
@@ -0,0 +1,10 @@
+export class Address {
+  constructor(
+    public streetNumber: string,
+    public street: string,
+    public postalcode: string,
+    public city: string,
+    public country: string
+  ) {
+  }
+}
