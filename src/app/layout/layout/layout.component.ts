207d033cc68ca798b4135d52428898c9cb0e204e
hhahhahh1 <hhahhahh@protonmail.com>
2024-06-20 11:07:59 +0300

    improve seo

---
+++ src/app/layout/layout/layout.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss']
+})
+export class LayoutComponent implements OnInit {
+
+  @Input() username: string;
+  @Input() isLoggedIn: boolean;
+  @Input() id: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
