87c729de5c9f2ce705c4576c198600ca1084e1c6
hhahhahh1 <hhahhahh@protonmail.com>
2024-07-12 11:07:59 +0300

    fix layout issue

---
+++ src/app/feature/account/my-animals/my-animals.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyAnimalsComponent } from './my-animals.component';
+
+describe('MyAnimalsComponent', () => {
+  let component: MyAnimalsComponent;
+  let fixture: ComponentFixture<MyAnimalsComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MyAnimalsComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyAnimalsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
