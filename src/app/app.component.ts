292662fe6af95674bf6009ee6d4d9c3de9cfc9e6
hhahhahh1 <hhahhahh@protonmail.com>
2024-07-22 11:07:59 +0300

    add new components

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { TokenStorageService } from './services/token-storage.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  title = 'animal-sitter-front';
+
+  private roles: string[];
+  isLoggedIn = false;
+  username: string;
+  id: string;
+
+
+  constructor(private tokenStorageService: TokenStorageService) { }
+
+  ngOnInit() {
+    this.isLoggedIn = !!this.tokenStorageService.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.tokenStorageService.getUser();
+      this.roles = user.roles;
+      this.username = user.username;
+      this.id = user.id;
+    }
+
+  }
+
+}
